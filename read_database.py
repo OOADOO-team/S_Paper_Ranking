2e638596426c37acc18c6959965fb207c7b3bfc0
Volan <32901290+Voldet@users.noreply.github.com>
2018-12-25 20:57:41 +0800

    draft

--- read_database.py
+++ read_database.py
@@ -1,18 +1,19 @@
 from openpyxl import load_workbook
-from bean.Paper import PaperBean
+import bean.Paper as p
 import re
+import rank as r
 
 
 # ================================================================
-def get_infomation(keyword):
+def get_infomation(keyword,alpha):
     """
         通过获取用户输入于界面的keyword,搜索数据库中author和title是否有吻合的关键词
         如果有吻合的关键词，则利用PaperBean构造出paper
         将paper加入result中，最后返回result list
+        支持模糊搜索
 
         :param keyword: 用户输入于界面的keyword
         :return result： paper list
-
     """
     wb = load_workbook("database.xlsx")
     sheet = wb.worksheets[0]
@@ -28,44 +29,18 @@ def get_infomation(keyword):
         judge1 = re.search(data_U, value_of_blockBn)
         judge2 = re.search(data_U, value_of_blockCn)
         if judge1 is not None or judge2 is not None:
-            paper = PaperBean(number=i,
-                              title=str(sheet.cell(row=i, column=2).value),
-                              authors=str(sheet.cell(row=i, column=3).value),
-                              published_in=str(sheet.cell(row=i, column=4).value),
-                              url=str(sheet.cell(row=i, column=6).value),
-                              abstract=str(sheet.cell(row=i, column=7).value),
-                              citations=sheet.cell(row=i, column=8).value,
-                              references=sheet.cell(row=i, column=9).value)
+            paper = p.PaperBean(number=i,
+                             title=str(sheet.cell(row=i+1, column=2).value),
+                             authors=str(sheet.cell(row=i+1, column=3).value),
+                            published_in=str(sheet.cell(row=i+1, column=4).value),
+                             url=str(sheet.cell(row=i+1, column=5).value),
+                             abstract=str(sheet.cell(row=i+1, column=6).value),
+                            citations_name=sheet.cell(row=i+1, column=7).value,
+                            citations_url=sheet.cell(row=i + 1, column=8).value,
+                             references_name=sheet.cell(row=i+1, column=9).value,
+                            reference_url = sheet.cell(row=i + 1, column=10).value,
+                            citation_number = sheet.cell(row=i + 1, column=11).value
+                              )
             result.append(paper)
-    return result
-
-
-def get_refer_and_cita(citation_list):
-    """
-        通过键入citation_list（对应paper的编号）
-        找寻对应citation_list中的paper
-        将paper加入result中，最后返回result list
-
-        :param citation_list: paper的编号
-        :return result： paper list
-
-    """
-    wb = load_workbook("E:\ChormeDownload\S_Paper_Ranking\database.xlsx")
-    sheet = wb.worksheets[0]
-    length = len(citation_list)
-    result = []
-    # get each line of xlsx
-    for i in range(length):
-        # 取得reference的编号，从而得到整篇paper
-        row_num = citation_list[i]
-        print(sheet.cell(row=row_num, column=1).value)
-        paper = PaperBean(number=sheet.cell(row=row_num, column=1).value,
-                          title=str(sheet.cell(row=row_num, column=2).value),
-                          authors=str(sheet.cell(row=row_num, column=3).value),
-                          published_in=str(sheet.cell(row=row_num, column=4).value),
-                          url=str(sheet.cell(row=row_num, column=6).value),
-                          abstract=str(sheet.cell(row=row_num, column=7).value),
-                          citations=sheet.cell(row=row_num, column=8).value,
-                          references=sheet.cell(row=row_num, column=9).value)
-        result.append(paper)
+        result = r.rank_simple(result,alpha)
     return result
--- view.py
+++ view.py
@@ -1,27 +1,28 @@
 from flask import Flask, request, render_template
 from bean.Paper import *
 from urllib.parse import quote
+import builtins
 from flask_bootstrap import Bootstrap
 
 
 p1 = PaperBean(number=1, title="paper1", authors='Wentao1', published_in='Sustech1', url='localhost:5000',
                abstract='this is the abstract of paper 1. this is the abstract of paper 1. this is the abstract of paper '
                         '1. this is the abstract of paper 1. this is the abstract of paper 1.',
-               citations=[1,2], references=[3,4])
+               citations=[1,2], references=[3,4], citation_number=0)
 p2 = PaperBean(number=2, title="paper2", authors='Wentao2', published_in='Sustech2', url='localhost:5000',
                abstract='this is the abstract of paper 2. this is the abstract of paper 2. this is the abstract of paper '
                         '2. this is the abstract of paper 2. this is the abstract of paper 2. this is the abstract of paper'
                         ' 2. this is the abstract of paper 2. this is the abstract of paper 2.',
-               citations=[1, 2], references=[3, 4])
+               citations=[1, 2], references=[3, 4],citation_number=0)
 p3 = PaperBean(number=3, title="paper3", authors='Wentao3', published_in='Sustech3', url='localhost:5000',
                abstract='this is the abstract of paper 3. this is the abstract of paper 3. this is the abstract of paper '
                         '3. this is the abstract of paper 3. this is the abstract of paper 3.',
-               citations=[1, 2], references=[3, 4])
+               citations=[1, 2], references=[3, 4],citation_number=0)
 p4 = PaperBean(number=4, title="paper4", authors='Wentao4', published_in='Sustech4', url='localhost:5000',
                abstract='this is the abstract of paper 4. this is the abstract of paper 4. this is the abstract of paper '
                         '4. this is the abstract of paper 4. this is the abstract of paper 4. this is the abstract of paper '
                         '4. this is the abstract of paper 4. this is the abstract of paper 4.',
-               citations=[1, 2], references=[3, 4])
+               citations=[1, 2], references=[3, 4],citation_number=0)
 
 end_result = {1: p1, 2: p2, 3: p3, 4: p4}
 
